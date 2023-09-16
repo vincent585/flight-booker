2089b6c7ae032ec2759f24b20c52ee8a30264904
Vincent Klimjack <vklimjack@gmail.com>
2023-09-16 11:14:19 +0100

    amend association for Airports to have arriving flights and departing flights

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -2,5 +2,6 @@
 
 # Airport model
 class Airport < ApplicationRecord
-  has_many :flights
+  has_many :arriving_flights, class_name: 'Flight'
+  has_many :departing_flights, class_name: 'Flight'
 end
