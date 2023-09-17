76fd7029ecd1e32d36fa4290e0e6cdc1682b64ba
Vincent Klimjack <vklimjack@gmail.com>
2023-09-17 20:15:44 +0100

    build form for flight search

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,5 +2,8 @@
 
 # class for handling flight search functionality
 class FlightsController < ApplicationController
-  def index; end
+  def index
+    @airport_options = Airport.all.map { |a| ["#{a.name} - #{a.iata_code}", a.iata_code] }
+    @departure_dates = Airport.all.flat_map { |a| a.departing_flights.map(&:departure_date) }
+  end
 end
