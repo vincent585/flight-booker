dadcd99a5e8f183ce8a615adb7c4a87f0832b570
Vincent Klimjack <vklimjack@gmail.com>
2023-10-27 13:20:56 +0100

    add error handling for same departure and arrival airports

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -5,15 +5,19 @@ class FlightsController < ApplicationController
   def index
     @airport_options = Airport.all.map { |a| ["#{a.name} - #{a.iata_code}", a.iata_code] }
     @departure_dates = Airport.all.flat_map { |a| a.departing_flights.map(&:departure_date) }
-    console
 
-    @flights = params[:commit].nil? ? Flight.first(5) : find_flights(flight_search_params)
+    @flights = flight_search_params.empty? ? Flight.first(5) : find_flights
   end
 
   private
 
-  def find_flights(params)
-    FlightFinder.new.find_flights(params)
+  def find_flights
+    if params[:arrival_airport] == params[:departure_airport]
+      flash.now[:error] = 'Arrival and departure airports must be different'
+      render :index, status: :unprocessable_entity
+    else
+      FlightFinder.new.find_flights(flight_search_params)
+    end
   end
 
   def flight_search_params
