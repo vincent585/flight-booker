766aba4c533a9db2c965d51bdb0534a5b12e272f
Vincent Klimjack <vklimjack@gmail.com>
2023-11-08 11:05:05 +0000

    add passenger count to flight finder

--- app/services/flight_finder.rb
+++ app/services/flight_finder.rb
@@ -3,11 +3,12 @@ class FlightFinder
     departure_date = search_params[:departure_date]
     departure_airport = Airport.find_by_iata_code(search_params[:departure_airport])
     arrival_airport = Airport.find_by_iata_code(search_params[:arrival_airport])
+    passenger_count = search_params[:passenger_count]
 
     Flight.where(
       departure_airport_id: departure_airport.id,
       arrival_airport_id: arrival_airport.id,
-      departure_date: departure_date
-    )
+      departure_date: departure_date,
+    ).where('seats_available >= ?', passenger_count)
   end
 end
