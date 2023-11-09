6cba3c4b7da37d51f420a1e5863ba78e4b354256
Vincent Klimjack <vklimjack@gmail.com>
2023-11-09 12:03:26 +0000

    fix db schema

--- db/migrate/20231108114406_create_bookings.rb
+++ db/migrate/20231108114406_create_bookings.rb
@@ -1,6 +1,6 @@
 class CreateBookings < ActiveRecord::Migration[7.0]
   def change
-    create_join_table :passengers, :flights, table_name: :bookings do |t|
+    create_table :bookings do |t|
       t.index :passenger_id
       t.index :flight_id
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_08_114406) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_09_112354) do
   create_table "airports", force: :cascade do |t|
     t.string "name"
     t.string "iata_code"
@@ -18,13 +18,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_08_114406) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "bookings", id: false, force: :cascade do |t|
-    t.integer "passenger_id", null: false
-    t.integer "flight_id", null: false
+  create_table "bookings", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["flight_id"], name: "index_bookings_on_flight_id"
-    t.index ["passenger_id"], name: "index_bookings_on_passenger_id"
+    t.index "\"flight_id\"", name: "index_bookings_on_flight_id"
+    t.index "\"passenger_id\"", name: "index_bookings_on_passenger_id"
   end
 
   create_table "flights", force: :cascade do |t|
@@ -42,6 +40,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_08_114406) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "booking_id"
   end
 
   add_foreign_key "flights", "airports", column: "arrival_airport_id"
